5779ba91311d1f2237db8c2db2febcaecfb30885
Ndelforno <nicolas.delforno@gmail.com>
2018-12-10 11:11:47 -0500

    exercise 3 done

--- practical_knowledge.rb
+++ practical_knowledge.rb
@@ -29,3 +29,19 @@ my_hash.each do |k,v|
 end
 
 p my_hash
+
+
+#3 Write a program that prints the numbers from 1 to 30.
+# But for multiples of two print “Word” instead of the number and for the multiples of five print “Jack”. For numbers which are multiples of both two and five print “WordJack”
+
+for i in 1..30 do
+  if i % 2 == 0 && i % 5 == 0
+    puts "WordJack"
+  elsif i % 5 == 0
+    puts "Jack"
+  elsif i % 2 == 0
+    puts "Word"
+  else
+    puts i
+  end
+end
