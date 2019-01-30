ae1dd4b9c0aa8a40563861a57c9f6ae31f3911d6
Ndelforno <nicolas.delforno@gmail.com>
2019-01-30 13:13:21 -0500

    question 3&4 done

--- lotum/lotum.rb
+++ lotum/lotum.rb
@@ -1,5 +1,5 @@
 
-
+#1
 def foobar_number()
   for i in 1..100 do
     if i % 3 == 0 && i % 5 == 0
@@ -16,7 +16,7 @@ end
 
 foobar_number()
 
-
+#2
 def count_even(n)
   count = 0
   for i in 0..n do
@@ -28,3 +28,13 @@ def count_even(n)
 end
 
 puts count_even(100)
+
+#3
+
+3..18
+
+#4
+
+words = ['one', 'one', 'two', 'three', 'three', 'two']
+
+p words.uniq
